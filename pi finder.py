7a97f174972b21f9376d4c7554dadba957b57b94
SWalters26 <124592626+SWalters26@users.noreply.github.com>
2023-02-22 21:41:36 +0000

    general code cleanup/organisation + comments
    reorganised the code and cleaned up stuff + added comments

--- pi finder.py
+++ pi finder.py
@@ -10,13 +10,10 @@ maths:
     if r<=1, circlecount += 1, totalcount += 1
     else, totalcount +=1
     area of circle = circlecount/totalcount * size of box (4)
-"""
-import numpy as np
-import random as rnd
+
+
 totalcount=0
 circlecount=0
-N=1000000   #number of random numbers 
-
 while totalcount <N:
     x=rnd.uniform(-1, 1)
     y=rnd.uniform(-1, 1)
@@ -27,13 +24,18 @@ while totalcount <N:
     #print(totalcount/10000)
 
 area = 4*(circlecount/totalcount) #gives the area of the square multiplied by the ratio of circle points to non-circle points
-
-uncertainty_estimate = 1/np.sqrt(N)
-#print(area, "+/-", uncertainty_estimate)
+"""
+import numpy as np
+import random as rnd
+N=1000000   #number of random numbers
+totalcount=0
+circlecount=0
+ 
+uncertainty_estimate = 1/np.sqrt(N)   #estimate of the uncertainty in V, proper calculation might be needed for consistent results
 
 # n-dimensional sphere volume/surface area calculations
-n = 5    # number of dimensions
-R = 1
+n = 4    # number of dimensions
+R = 1    # radius
 
 #Recurively calculating Vn(R)
 def recursive_V(n):  #works for any d > 0
@@ -43,28 +45,26 @@ def recursive_V(n):  #works for any d > 0
         return 2*R   #1D case
     else:
         return (2*np.pi/n * R**2 * recursive_V(n-2))   #equation taken from 2d recurrance relation on wikipedia page for volume of an n ball
-        
-s_Area = (n/R)*recursive_V(n)  #Surface area of n-d sphere 
-
-totalcount1=0
-circlecount1=0
-
-while totalcount1 < N:
-    pos_R = np.ones((n,1), dtype='float')
-    total = 0 
-    for i in range(n):
-        pos_R[i-1] = [rnd.uniform(-1,1)]   #creating an array with n random numbers stored
-        total += pos_R[i-1]**2
-    r = np.sqrt(total) #square rooting over the sum of all random numbers squared
-    if r <= 1:
-        circlecount1 += 1
-    totalcount1 += 1
-
+    
 #used for calculating the area of the n-dimensional box
 def double(n):  #returns 2x the last value of n. i.e. n = 2 give 4, n = 3 gives 8 
     if n == 0:
         return 1
     else:
         return 2*double(n-1)
+        
+s_Area = (n/R)*recursive_V(n)  #Surface area of n-d sphere, this is currently being used not entirely sure if we need it or not
+
+while totalcount < N:
+    pos_R = np.ones((n, 1), dtype='float')
+    total = 0 
+    for i in range(n):
+        pos_R[i-1] = [rnd.uniform(-1, 1)]   #creating an array with n random numbers stored
+        total += float(pos_R[i-1]**2)
+    r = np.sqrt(total) #square rooting over the sum of all random numbers squared
+    if r <= 1:
+        circlecount += 1
+    totalcount += 1
     
-n_area = double(n)*(circlecount1/totalcount1)   #area of box multiplied by the ratio of points in the sphere vs outside
+n_area = double(n)*(circlecount/totalcount)   #area of box multiplied by the ratio of points in the sphere vs outside
+print(n_area, "+/-", uncertainty_estimate, "value obtained via monte-carlo integration, compared to the actual value of", recursive_V(n))
