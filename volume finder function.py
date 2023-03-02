b8fea8dab0fb00c8f86e466879f60784356b3216
SWalters26 <124592626+SWalters26@users.noreply.github.com>
2023-03-02 12:51:43 +0000

    general cleanup
    re-ordered the variable names in the functions and adjusted the number of runs to 10,000,000 just to test it. It takes about 3 mins to run for D=2 but gives a very good answer (3.1415) with the errors correct. Want to test if the error will be correct here for 10D but will take ages to run

--- volume finder function.py
+++ volume finder function.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 def Volume(r,D):
-    N = 1000000
+    N = 10000000 # this is incredibly accurate for N = 10,000,000 but it takes like 3 mins to run for 2d 
     
-    def trueV(D, r):  #works for any d > 0
+    def trueV(r, D):  #works for any d > 0
         if D == 0:
             return 1   #0D case
         elif D == 1:
@@ -11,14 +11,14 @@ def Volume(r,D):
         else:
             return (2*np.pi/D * r**2 * trueV(D-2, r))
 
-    def ranradii(D,r,N):
+    def ranradii(r,D,N):
         arr=np.zeros([N,1])
         for i in range(N):
-            arr[i]=float(np.sqrt(sum(np.random.uniform(-r,r,size=(D,1))**2)))
+            arr[i]=float(np.sqrt(sum(np.random.uniform(-r,r,size=(D,1))**2))) 
         return arr
 
-    def volfind(D,r,N):
-        xi=ranradii(D,r,N)   
+    def volfind(r,D,N):
+        xi=ranradii(r,D,N)   
         totalcount=0
         circlecount=0
         while totalcount < N:
@@ -28,8 +28,8 @@ def Volume(r,D):
         vol=circlecount/totalcount * (2*r)**D            
         return vol
     
-    def volerr(D,r,N):
-        error = np.sqrt((1/(N*(N-1)))*sum((ranradii(D,r,N) - volfind(D,r,N))**2))
+    def volerr(r,D,N):
+        error = np.sqrt((1/(N*(N-1)))*sum((ranradii(r,D,N) - volfind(r,D,N))**2))
         return float(error)
 
-    print("for a sphere in",D,"dimesnions, with radius",r,", volume =",volfind(D,r,N),"+/-",volerr(D,r,N),"compared to the real value of,",trueV(D,r))
+    print("for a sphere in",D,"dimesnions, with radius",r,", volume =",volfind(r,D,N),"+/-",volerr(r,D,N),"compared to the real value of,",trueV(r,D))
