1fa0c49d441b4ec9bc3e571758f109c7a01926ad
SWalters26 <124592626+SWalters26@users.noreply.github.com>
2023-03-02 14:46:27 +0000

    Seperate function made to find the results requested in the assignment
    Made a seperate function to find the values asked for in the assignment. Also tried saving those results into their own datastructures to make it easier to save these to file which is the next step. When doing so, another column will need to be added specifying the change made between each line item in the lists.

--- volume finder function.py
+++ volume finder function.py
@@ -1,15 +1,29 @@
 import numpy as np
 
+def assignment():
+    #1) 
+    results_1 = []
+    for D in range(1,11):
+        results_1.append(Volume(1,D))
+        #print(results_1)
+        
+    #2)
+    results_2 = []
+    for r in np.arange(1,5.5,0.5):
+        results_2.append(Volume(r,3))
+        
+    return results_1, results_2
+
 def Volume(r,D):
-    N = 10000000 # this is incredibly accurate for N = 10,000,000 but it takes like 3 mins to run for 2d 
+    N = 1000000 # this is incredibly accurate for N = 10,000,000 but it takes like 3 mins to run for 2d 
     
-    def trueV(r, D):  #works for any d > 0
+    def trueV(r,D):  #works for any d > 0
         if D == 0:
             return 1   #0D case
         elif D == 1:
             return 2*r   #1D case
         else:
-            return (2*np.pi/D * r**2 * trueV(D-2, r))
+            return (2*np.pi/D * r**2 * trueV(r, D-2))
 
     def ranradii(r,D,N):
         arr=np.zeros([N,1])
@@ -32,4 +46,5 @@ def Volume(r,D):
         error = np.sqrt((1/(N*(N-1)))*sum((ranradii(r,D,N) - volfind(r,D,N))**2))
         return float(error)
 
-    print("for a sphere in",D,"dimesnions, with radius",r,", volume =",volfind(r,D,N),"+/-",volerr(r,D,N),"compared to the real value of,",trueV(r,D))
+    return [volfind(r,D,N), volerr(r,D,N)]
+    #print("for a sphere in",D,"dimesnions, with radius",r,", volume =",volfind(r,D,N),"+/-",volerr(r,D,N),"compared to the real value of,",trueV(r,D))
