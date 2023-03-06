47cc6a36ae6badc2b85e62b415054f56c9c9bf14
SWalters26 <124592626+SWalters26@users.noreply.github.com>
2023-03-06 15:04:52 +0000

    error thing???
    error thing that works better standard deviation in random number

--- volume finder function.py
+++ volume finder function.py
@@ -1,33 +1,7 @@
 import numpy as np
 
-def assignmentest():
-    #1) 
-    results_1 = np.zeros((10,3), dtype='float')
-    for D in range(1,11):
-        temp = np.array_split(Volume(1,D),2)
-        results_1[D-1,:] = [D,temp[0],temp[1]]
-        print(results_1)
-    
-    #np.savetxt("D data.txt", results_1)
-    '''    #unable to get this to work for now will try again soon but the need 
-    # for this to be an arange is causing problems when trying to reslice the arrays back into 1 array
-    #1 possible solution for this is to avoid using floats casting the loop and 
-    # instead just doing 2r then doing 2r/2 where needed but this doesn't quite feel right although could be done as a last resort
-    
-    #2)
-    results_2 = np.empty((9,3), dtype='float')
-    for r in np.arange(1,5.5,0.5):
-        results_2[r-1,:] = np.concatenate((results_2, np.array_split(Volume(r,3),2)),axis=1)
-        #temp = np.array_split(Volume(r,3),2)   #splits output of Volume into 2 arrays, 1 for volume the other for error
-        #results_2[r-1,:] = np.array([r,temp[0],temp[1]])
-        print(results_2)
-    '''    
-    return results_1, #results_2
-
-
-
 def Volume(r,D):
-    N = 10000 # this is incredibly accurate for N = 10,000,000 but it takes like 3 mins to run for 2d 
+    N = 1000 # this is incredibly accurate for N = 10,000,000 but it takes like 3 mins to run for 2d 
     
     def trueV(r,D):  #works for any d > 0
         if D == 0:
@@ -52,16 +26,12 @@ def Volume(r,D):
                 circlecount +=1
             totalcount += 1
         vol=circlecount/totalcount * (2*r)**D
-        error = float(np.sqrt((1/(N*(N-1)))*sum((xi - vol)**2)))#folded the error function into the main finder and used the array of variables used for finding the volume            
+        #error = float(np.sqrt((1/(N*(N-1)))*sum((xi - vol)**2)))#folded the error function into the main finder and used the array of variables used for finding the volume            
+        error = np.sqrt((1/(N*(N-1)))*np.abs(sum(xi - (1/N*sum(xi)**2))))
         return vol,error #print("volume =",vol,"+/-",error)
     
-    #def volerr(r,D,N):
-        #error = np.sqrt((1/(N*(N-1)))*sum((ranradii(r,D,N) - volfind(r,D,N))**2))
-        #return float(error)
 
     return volfind(r,D,N)
-    #print("for a sphere in",D,"dimesnions, with radius",r,", volume =",volfind(r,D,N),"+/-",volerr(r,D,N),"compared to the real value of,",trueV(r,D))
-
 
 def assignment():
     dimarr=np.zeros([10,3])
